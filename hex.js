bf2e7cd022ba50fde7ce041d4522bf9f36823236
Craig Fitzgerald <craig.fitzgerald00@gmail.com>
2022-11-23 16:35:37 -0500

    new element generator

--- hex.js
+++ hex.js
@@ -2,10 +2,8 @@
 
 // hex.js
 //
-// Generate random image of hexagonal tilings
+// Generate image of random hexagonal tilings
 // This is also an example of saving a canvas as a png
-//
-// todo: create some good tile generators
 //
 // Craig Fitzgerald 2022
 
@@ -22,8 +20,7 @@ class HexTiler {
       $(window       ).on("resize",()=>this.Resize());
       $("#regenerate").on("click", ()=>this.Regen());
       $("#download"  ).on("click", ()=>this.Download());
-      this.InitStyles();
-      this.InitElements();
+      this.InitState();
       this.Resize();
    }
 
@@ -36,56 +33,93 @@ class HexTiler {
       this.canvas.height = y;
       this.xGrid = Math.floor(x / this.dx) + 1;
       this.yGrid = Math.floor(y / this.dy) + 1;
+
       this.Draw();
    }
 
    Regen() {
-      this.InitStyles();
-      this.InitElements();
+      this.InitState();
       this.Draw();
    }
 
-   InitStyles() {
-      let h = Math.random() * 360;
+   InitState() {
+      // hex size
+      this.radius = this.R(150, 30);
+      this.dx  = this.radius * 2;
+      this.dy  = this.radius * 2 * 0.86602;
+      this.xGrid = Math.floor($(window).width()  / this.dx) + 1;
+      this.yGrid = Math.floor($(window).height() / this.dy) + 1;
+
+      // colors
+      let h = this.R(360);
       this.styles = [
-         this.HSL (h,     "35%", "55%"),
-         this.HSL ((h+180) % 360, "95%", "35%"),
-         this.HSL ((h+120) % 360, "95%", "35%"),
-         this.HSL ((h+240) % 360, "95%", "35%"),
+         this.HSL (h,             this.RI(90, 10), this.RI(60, 29)),
+         this.HSL ((h+180) % 360, this.RI(90, 10), this.RI(90, 10)),
+         this.HSL ((h+120) % 360, this.RI(90, 10), this.RI(90, 10)),
+         this.HSL ((h+240) % 360, this.RI(90, 10), this.RI(90, 10)),
       ];
+
+      // elements
+      this.elements = [];
+      let count =this.RI(8,2);
+      for (let i=0; i<count; i++) {
+         this.elements.push(this.CreateElement());
+      }
    }
 
-   InitElements() {
-      let r  = this.radius;
-      let r0 = this.Rand(4, r, 0);
-      let r1 = this.Rand(4, r * 1.5,  - r/2);
-      let r2 = this.Rand(4, r * 1.25, - r/2);
-      let r3 = this.Rand(4, r, - r/2);
+   CreateElement() {
+     let r =this.RI(3);
+      switch (r) {
+         case 0: return this.CreateCircle();
+         case 1: return this.CreateRect();
+         case 2: return this.CreateLine();
+      }
+   }
 
-      this.elements = [
-         {s: 1, f: () => this.Circle(0   , 0 , r0[0], 5, 1,0)},
-         {s: 6, f: () => this.Circle(r0[1], r0[2], r0[3]/4, 5, 0, 2)},
-         {s: 6, f: () => this.Rect  (-10 , 10, 100, 5, 2)},
-         {s: 6, f: () => this.Rect  (r * 0.8, -r * 0.4, 10, r * 0.8, 3)},
-         {s: 6, f: () => this.Circle(r * 0.8, 0, 10, 5, 0, 3)},
-      ];
-      // todo... flesh out some good generators
-      this.elements.push({s: 6, f: () => this.Rect (...r1, 2)});
-      this.elements.push({s: 6, f: () => this.Rect (...r2, 3)});
-      this.elements.push({s: 6, f: () => this.Rect (...r3, 1)});
+   CreateCircle() {
+      let rand = this.RI(10);
+      let sym = rand==0 ? 1 : rand<5 ? 3 : 6;
+      let center = sym==1 ? [0,0] : this.Rand(2, this.radius, 0);
+      let rad = this.R(this.radius);
+      let color = this.RI(3, 1);
+      rand = this.RI(13,1);
+      let width = rand>10 ? 0 : rand;
+
+      return {sym, f:this.Circle, p:[...center, rad, width, color]};
+   }
+
+   CreateRect() {
+      let rad  = this.radius;
+      let pos = this.Rand(4, rad * 2.5, -rad);
+      let sym = this.RI(2)==0 ? 3 : 6;
+      let rand = this.RI(19,1);
+      let width = rand>10 ? 0 : rand;
+      let color = this.RI(3, 1);
+
+      return {sym, f:this.Rect, p:[...pos, width, color]};
+   }
+
+   CreateLine() {
+      let rad  = this.radius;
+      let pos = this.Rand(4, rad * 2, -rad);
+      let sym = this.RI(2)==0 ? 3 : 6;
+      let rand = this.RI(19,1);
+      let width = rand>10 ? 0 : rand;
+      let color = this.RI(3, 1);
+
+      return {sym, f:this.Line, p:[...pos, width, color]};
    }
 
    Draw() {
       this.DrawBackground();
-
-      // account for elements possibly exceeding the boundry of a hex
-      for (let y = -1; y <= this.yGrid; y++) {
-         for (let x = -1; x <= this.xGrid; x++) {
-            let xOffset = y % 2 ? 0 : this.radius;
-            let c = {x: x*this.dx + xOffset, y: y*this.dy + this.radius};
-            this.DrawHex(c);
+      this.elements.forEach((e) => {
+         for (let y = -1; y <= this.yGrid; y++) {
+            for (let x = -1; x <= this.xGrid; x++) {
+               let xOffset = y % 2 ? 0 : this.radius;
+               this.DrawHex(e, x*this.dx + xOffset, y*this.dy + this.radius);
+            }
          }
-      }
+      });
    }
 
    DrawBackground() {
@@ -93,55 +127,68 @@ class HexTiler {
       this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
    }
 
-   DrawHex(c) {
+   DrawHex(e, x, y) {
       this.ctx.save();
-      this.ctx.translate(c.x, c.y);
+      this.ctx.translate(x, y);
 
-      this.elements.forEach((e) => {
-         if (e.s == 1) {
-            e.f();
-         }
-         if (e.s == 6) {
-            for (let i=0; i<6; i++) {
-               this.DrawSliver(e, i);
-            }
-         }
-      });
+      for (let i=0; i<e.sym; i++) {
+         this.DrawElement(e, i*(e.sym==3 ? 2 : 1));
+      }
       this.ctx.restore();
    }
 
-   DrawSliver(e, i) {
+   DrawElement(e, rot) {
       this.ctx.save();
-      this.ctx.rotate(i * Math.PI / 3);
-      e.f();
+      if (rot) this.ctx.rotate(rot * Math.PI / 3);
+      e.f.call(this,...e.p);
       this.ctx.restore();
    }
 
-   Circle (x, y, r, w, ss, fs) {
+   Circle(x, y, rad, width, style) {
       this.ctx.beginPath()
-      this.ctx.arc(x,y,r,0,Math.PI * 2);
-      this.ctx.strokeStyle = this.styles[ss];
-      this.ctx.fillStyle = this.styles[fs];
-      this.ctx.lineWidth = w;
-      if (ss) this.ctx.stroke();
-      if (fs) this.ctx.fill();
+      this.ctx.arc(x,y,rad,0,Math.PI * 2);
+      this.ctx.strokeStyle = this.styles[style];
+      this.ctx.fillStyle = this.styles[style];
+      this.ctx.lineWidth = width;
+      if (width) this.ctx.stroke();
+      if (!width) this.ctx.fill();
    }
 
-   Rect (x, y, w, h, c) {
-      this.ctx.fillStyle = this.styles[c];
-      this.ctx.fillRect(x, y, w, h);
+   Rect(x, y, width, height, lineWidth, style) {
+      this.ctx.strokeStyle = this.styles[style];
+      this.ctx.fillStyle = this.styles[style];
+      this.ctx.lineWidth = lineWidth;
+      if (lineWidth) this.ctx.strokeRect(x, y, width, height);
+      if (!lineWidth)this.ctx.fillRect(x, y, width, height);
    }
 
-   Rand(count, size, offset) {
+   Line(x0, y0, x1, y1, lineWidth, style) {
+      this.ctx.strokeStyle = this.styles[style];
+      this.ctx.lineWidth = lineWidth;
+      this.ctx.beginPath();
+      this.ctx.moveTo(x0, y1);
+      this.ctx.lineTo(x1, y1);
+      this.ctx.stroke();
+   }
+
+   R (max=1, offset=0) {
+      return offset + Math.random() * max;
+   }
+
+   RI (max, offset=0) {
+      return Math.floor(offset + Math.random() * max);
+   }
+
+   Rand(count, max=1, offset=0) {
       let ret = [];
       for (let i=0; i<count; i++) {
-         ret.push(Math.random() * size + offset);
+         ret.push(Math.random() * max + offset);
       }
       return ret;
    }
 
    HSL(h, s, l){
-      return 'hsl('+h+','+s+','+l+')';
+      return 'hsl('+h+','+s+'%,'+l+'%)';
    }
 
    Download() {
